1eade2d33791f757bc4503d9932807e86bb3fc1e
IKLANLO <ilandaberea@yahoo.es>
2024-01-31 18:30:35 +0100

    modificados props de Header

--- Ejercicio_2/src/App.jsx
+++ Ejercicio_2/src/App.jsx
@@ -4,23 +4,24 @@ import './App.css'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 
+const name = 'Iker Landaberea Lorenzo'
+const age = 39
+
 function App() {
   return (
     <>
-      <Header name='Iker Landaberea Lorenzo' age='39'/>
+      <Header name={name} age={age}/>
       <Home 
-        section='¿Quién soy?' 
-        data='Tal y como indica el encabezado, soy Iker, tengo 39 años, y actualmente estoy cursando un bootcamp de desarrollo web 
-            en la modalidad part-time.'
+        section={'¿Quién soy?'} 
+        data={'Tal y como indica el encabezado, soy Iker, tengo 39 años, y actualmente estoy cursando un bootcamp de desarrollo web en la modalidad part-time.'}
       />
       <Home 
-        section='¿Qué estudios tengo?' 
-        data='Soy técnico superior en administración de sistemas informáticos, y también diplomado en empresariales.'
+        section={'¿Qué estudios tengo?' }
+        data={'Soy técnico superior en administración de sistemas informáticos, y también diplomado en empresariales.'}
       />
       <Home 
-        section='¿Qué tecnologías conozco?' 
-        data='En el bootcamp, hasta el momento he aprendido a utilizar HTML, CSS, Javascript, mySQL, MongoDB, Bootstrap, Docker, 
-              Node.js, Swagger, y actualmente aprendiendo React'
+        section={'¿Qué tecnologías conozco?'} 
+        data={'En el bootcamp, hasta el momento he aprendido a utilizar HTML, CSS, Javascript, mySQL, MongoDB, Bootstrap, Docker, Node.js, Swagger, y actualmente aprendiendo React'}
       />
     </>
   )
